05597507d1cb2d689f613b33f99d885c5ed1f4a0
UuuNyaa <uuunyaa@gmail.com>
2023-12-07 03:08:39 -0800

    Prepare for Blender v4 support

--- mmd_tools/__init__.py
+++ mmd_tools/__init__.py
@@ -3,7 +3,7 @@
 bl_info = {
     "name": "mmd_tools",
     "author": "sugiany",
-    "version": (2, 10, 0),
+    "version": (2, 10, 1),
     "blender": (2, 93, 0),
     "location": "View3D > Sidebar > MMD Tools Panel",
     "description": "Utility tools for MMD model editing. (UuuNyaa's forked version)",
