86c65fe9a21d207bb3b0296de4a24a913b346aec
rintrint <152774318+rintrint@users.noreply.github.com>
2025-05-29 22:37:09 +0900

    Update importer.py

--- mmd_tools/core/pmx/importer.py
+++ mmd_tools/core/pmx/importer.py
@@ -792,6 +792,8 @@ class PMXImporter:
         # Without sharp edges, Blender will automatically interpolate/smooth normals between
         # adjacent faces, causing custom normals to be modified or ignored entirely.
         current_mode = bpy.context.object.mode
+        bpy.ops.object.mode_set(mode="OBJECT")
+        bpy.ops.object.select_all(action="DESELECT")
         bpy.context.view_layer.objects.active = self.__meshObj
         bpy.ops.object.mode_set(mode="EDIT")
         bpy.ops.mesh.select_all(action="SELECT")
